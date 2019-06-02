28fd4464705decff97aaa3ab92796931f6c071aa
panicbit <panicbit.dev@gmail.com>
2019-06-02 16:10:28 +0200

    Improve raw borrowing

--- src/borrow_raw.rs
+++ src/borrow_raw.rs
@@ -2,50 +2,64 @@
 pub trait BorrowRaw<T> {
     unsafe fn borrow_raw<F, R>(&self, f: F) -> Option<R>
     where
-        F: FnOnce(&T) -> R;
+        F: FnOnce(&T) -> R
+    {
+        self.borrow_raw_opt(|value| value.map(f))
+    }
+
+    unsafe fn borrow_raw_opt<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(Option<&T>) -> R;
 }
 
 impl<T> BorrowRaw<T> for *const T {
-    unsafe fn borrow_raw<F, R>(&self, f: F) -> Option<R>
+    unsafe fn borrow_raw_opt<F, R>(&self, f: F) -> R
     where
-        F: FnOnce(&T) -> R
+        F: FnOnce(Option<&T>) -> R
     {
         if self.is_null() {
-            return None;
+            f(None)
+        } else {
+            f(Some(&**self))
         }
-
-        Some(f(&**self))
     }
 }
 
 impl<T> BorrowRaw<T> for *mut T {
-    unsafe fn borrow_raw<F, R>(&self, f: F) -> Option<R>
+    unsafe fn borrow_raw_opt<F, R>(&self, f: F) -> R
     where
-        F: FnOnce(&T) -> R
+        F: FnOnce(Option<&T>) -> R
     {
         if self.is_null() {
-            return None;
+            f(None)
+        } else {
+            f(Some(&**self))
         }
-
-        Some(f(&**self))
     }
 }
 
 pub trait BorrowRawMut<T> {
     unsafe fn borrow_raw_mut<F, R>(&self, f: F) -> Option<R>
     where
-        F: FnOnce(&mut T) -> R;
+        F: FnOnce(&mut T) -> R
+    {
+        self.borrow_raw_mut_opt(|value| value.map(f))
+    }
+
+    unsafe fn borrow_raw_mut_opt<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(Option<&mut T>) -> R;
 }
 
 impl<T> BorrowRawMut<T> for *mut T {
-    unsafe fn borrow_raw_mut<F, R>(&self, f: F) -> Option<R>
+    unsafe fn borrow_raw_mut_opt<F, R>(&self, f: F) -> R
     where
-        F: FnOnce(&mut T) -> R
+        F: FnOnce(Option<&mut T>) -> R
     {
         if self.is_null() {
-            return None;
+            f(None)
+        } else {
+            f(Some(&mut **self))
         }
-
-        Some(f(&mut **self))
     }
 }
